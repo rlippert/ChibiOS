15aea5126b693dbd3fb87e4628e69fa2c86d8363
Rob Lippert <roblip@gmail.com>
2016-05-10 19:09:53 -0700

    hal/ports/avr: add USB device-mode driver for USB capable AVRs
    Adds support for USB device functionality for AT90USB and
    ATU2/U4 series devices.
    
    Support tested on a PJRC TEENSY2++ board with AT90USB1286.
    
    Signed-off-by: Rob Lippert <roblip@gmail.com>

--- os/hal/ports/AVR/platform.mk
+++ os/hal/ports/AVR/platform.mk
@@ -8,7 +8,8 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/ports/AVR/hal_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/hal_gpt_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/hal_pwm_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/hal_icu_lld.c \
-              ${CHIBIOS}/os/hal/ports/AVR/hal_st_lld.c
+              ${CHIBIOS}/os/hal/ports/AVR/hal_st_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_usb_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/AVR
